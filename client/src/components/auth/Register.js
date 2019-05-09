88b058013c42a3f84b2674115be33bc0fda6a23a
JoAn <joannacalinska@gmail.com>
2019-05-09 21:47:31 +0200

    login component

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,4 +1,6 @@
-import React, { Fragment, useState } from 'react'
+import React, { Fragment, useState } from 'react';
+import { Link } from 'react-router-dom';
+
 
 const Register = props => {
   const [formData, setFormData] = useState({
@@ -73,11 +75,11 @@ const Register = props => {
         <input type="submit" className="btn btn-primary" value="Register" />
       </form>
       <p className="my-1">
-        Already have an account? <a href="login.html">Sign In</a>
+        Already have an account? <Link to="/login">Sign In</Link>
       </p>
     </Fragment>
   );
 };
 
 
-export default Register
+export default Register;
