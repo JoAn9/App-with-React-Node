25384ed5e593741f58683e38043cd9bbd37bb5c2
JoAn <joannacalinska@gmail.com>
2019-06-05 22:32:28 +0200

    display error messages on FE

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -14,20 +14,16 @@ export const register = ({ name, email, password }) => async dispatch => {
   const body = JSON.stringify({ name, email, password });
 
   try {
-    const res = await axios.post('/api/users/register', body, config);
-    console.log('res in action: ', res);
+    const res = await axios.post('/api/users', body, config);
     dispatch({
       type: REGISTER_SUCCESS,
       payload: res.data,
     });
   } catch (err) {
-    console.log('err.response: ', err.response)
-    const errors = err.response.data;
+    const errors = err.response.data.errors;
+    
     if (errors) {
-      const errorsArray = Object.entries(errors);
-
-      // ToDo: get array with errors from BE
-      errorsArray.forEach(error => dispatch(setAlert(error[1], 'danger')));
+      errors.forEach(error => dispatch(setAlert(error.msg, 'danger')));
     }
 
     dispatch({
