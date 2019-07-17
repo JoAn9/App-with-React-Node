24c9060f4cc3a3f3bb18d937f151f6c7aeb31b3c
JoAn <joannacalinska@gmail.com>
2019-07-17 12:51:03 +0200

    dashboard

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -41,7 +41,7 @@ const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
   )
 };
 
-Navbar.PropTypes = {
+Navbar.propTypes = {
   logout: PropTypes.func.isRequired,
   auth: PropTypes.object.isRequired,
 }
