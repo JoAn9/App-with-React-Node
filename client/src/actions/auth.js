c4b103d0ccb4573f9318548ea7f076a9f72cd263
JoAn <joannacalinska@gmail.com>
2019-07-19 11:04:30 +0200

    clear profile - action and reducer

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -7,7 +7,8 @@ import {
   AUTH_ERROR,
   LOGIN_SUCCESS,
   LOGIN_FAIL,
-  LOGOUT
+  LOGOUT,
+  CLEAR_PROFILE,
 } from './types';
 import setAuthToken from '../utils/setAuthToken';
 
@@ -87,15 +88,12 @@ export const login = (email, password) => async dispatch => {
       errors.forEach(error => dispatch(setAlert(error.msg, 'danger')));
     }
 
-    dispatch({
-      type: LOGIN_FAIL,
-    });
+    dispatch({ type: LOGIN_FAIL });
   }
 }
 
 // Logout User
 export const logout = () => dispatch => {
-  dispatch({
-    type: LOGOUT,
-  });
+  dispatch({ type: LOGOUT });
+  dispatch({ type: CLEAR_PROFILE });
 }
--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -7,16 +7,11 @@ export const getCurrentProfile = () => async dispatch => {
   try {
     const res = await axios.get('./api/profile/me');
 
-    console.log('res: ', res);
-
     dispatch({
       type: GET_PROFILE,
       payload: res.data,
     })
   } catch (err) {
-
-    console.log('err: ', err);
-
     dispatch({
       type: PROFILE_ERROR,
       payload: {
