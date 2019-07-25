3a92f1050602c9e99aa8d3c7056887131bdac585
JoAn <joannacalinska@gmail.com>
2019-07-25 12:12:45 +0200

    experience list

---
+++ client/src/components/dashboard/Experience.js
@@ -0,0 +1,43 @@
+import React, { Fragment } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import Moment from 'react-moment';
+
+const Experience = ({ experience }) => {
+  const experiences = experience.map(item => (
+    <tr key={item._id}>
+      <td>{item.company}</td>
+      <td className="hide-sm">{item.title}</td>
+      <td className="hide-sm">
+        <Moment format="YYYY/MM">{item.from}</Moment> -{' '}
+        {item.to === null ? 'Now' : <Moment format="YYYY/MM">{item.to}</Moment>}
+      </td>
+      <td>
+        <button className="btn btn-danger">Delete</button>
+      </td>
+    </tr>
+  ));
+
+  return (
+    <Fragment>
+      <h2 className="my-2">Experience</h2>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Company</th>
+            <th className="hide-sm">Title</th>
+            <th className="hide-sm">Years</th>
+            <th />
+          </tr>
+        </thead>
+        <tbody>{experiences}</tbody>
+      </table>
+    </Fragment>
+  );
+};
+
+Experience.propTypes = {
+  experience: PropTypes.array.isRequired,
+};
+
+export default Experience;
