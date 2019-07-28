df3ab4b32d6a49deb68764f292e5f09c1fb12e82
JoAn <joannacalinska@gmail.com>
2019-07-28 20:54:50 +0200

    profiles component

---
+++ client/src/components/profiles/Profiles.js
@@ -0,0 +1,50 @@
+import React, { useEffect, Fragment } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import Spinner from '../layout/Spinner';
+import ProfileItem from './ProfileItem';
+import { getProfiles } from '../../actions/profile';
+
+const Profiles = ({ getProfiles, profile: { profiles, loading } }) => {
+  useEffect(() => {
+    getProfiles();
+  }, [getProfiles]);
+  return (
+    <Fragment>
+      {loading ? (
+        <Spinner />
+      ) : (
+        <Fragment>
+          <h1 className="large text-primary">Developers</h1>
+          <p className="lead">
+            <i className="fab fa-connectdevelop" /> Browse and connect with
+            developers
+          </p>
+          <div className="profiles">
+            {profiles.length > 0 ? (
+              profiles.map(item => (
+                <ProfileItem key={item._id} profile={item} />
+              ))
+            ) : (
+              <h4>No profiles found</h4>
+            )}
+          </div>
+        </Fragment>
+      )}
+    </Fragment>
+  );
+};
+
+Profiles.propTypes = {
+  getProfiles: PropTypes.func.isRequired,
+  profile: PropTypes.object.isRequired,
+};
+
+const mapStateToProps = state => ({
+  profile: state.profile,
+});
+
+export default connect(
+  mapStateToProps,
+  { getProfiles }
+)(Profiles);
