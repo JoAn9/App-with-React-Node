8097a50f4c1ae8606ec5410e73ec2d202abe86a5
JoAn <joannacalinska@gmail.com>
2019-08-02 21:40:57 +0200

    Posts component

--- client/src/components/post/Posts.js
+++ client/src/components/post/Posts.js
@@ -1,13 +1,30 @@
 import React, { Fragment, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import PostItem from './PostItem';
+import Spinner from '../layout/Spinner';
 import { getPosts } from '../../actions/post';
 
-const Posts = ({ getPosts, post: { posts, loading } }) => {
+const Posts = ({ getPosts, post: { posts, post, loading } }) => {
   useEffect(() => {
     getPosts();
   }, [getPosts]);
-  return <div />;
+  return loading ? (
+    <Spinner />
+  ) : (
+    <Fragment>
+      <h1 className="large text-primary">Posts</h1>
+      <p className="lead">
+        <i className="fas fa-user" /> Welcome to the community
+      </p>
+      <div className="posts">
+        {/* @todo: check when no posts */}
+        {posts.map(item => (
+          <PostItem key={item._id} post={item} />
+        ))}
+      </div>
+    </Fragment>
+  );
 };
 
 Posts.propTypes = {
