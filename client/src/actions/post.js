72298024bdf575df85a736ccdb3c4dcf6b2847cc
JoAn <joannacalinska@gmail.com>
2019-08-07 21:15:30 +0200

    add comment action

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -6,6 +6,8 @@ import {
   DELETE_POST,
   ADD_POST,
   GET_POST,
+  ADD_COMMENT,
+  REMOVE_COMMENT,
 } from './types';
 import { setAlert } from './alert';
 
@@ -115,13 +117,42 @@ export const addPost = formData => async dispatch => {
 
 // Get post
 export const getPost = id => async dispatch => {
-  console.log('id in act: ', id);
   try {
     const res = await axios.get(`/api/posts/${id}`);
     dispatch({
       type: GET_POST,
       payload: res.data,
     });
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: {
+        msg: err.response.statusText,
+        status: err.response.status,
+      },
+    });
+  }
+};
+
+// Add comment
+export const addComment = (postId, formData) => async dispatch => {
+  const config = {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  };
+  try {
+    const res = await axios.post(
+      `/api/posts/comment/${postId}`,
+      formData,
+      config
+    );
+    dispatch({
+      type: ADD_COMMENT,
+      payload: res.data,
+    });
+
+    dispatch(setAlert('Comment added', 'success'));
   } catch (err) {
     dispatch({
       type: POST_ERROR,
