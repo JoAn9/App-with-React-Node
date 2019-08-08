265f9319e7cb598382643fa76d506fa6d81eb288
JoAn <joannacalinska@gmail.com>
2019-08-08 23:15:24 +0200

    comment item component

--- client/src/components/post/CommentItem.js
+++ client/src/components/post/CommentItem.js
@@ -1,33 +1,53 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import Moment from 'react-moment';
+import { deleteComment } from '../../actions/post';
 
-const CommentItem = ({ comment }) => {
-  console.log(comment);
-  return (
-    <div className="post bg-white p-1 my-1">
-      <div>
-        <a href="profile.html">
-          <img
-            className="round-img"
-            src="https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50?s=200"
-            alt=""
-          />
-          <h4>John Doe</h4>
-        </a>
-      </div>
-      <div>
-        <p className="my-1">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint possimus
-          corporis sunt necessitatibus! Minus nesciunt soluta suscipit nobis.
-          Amet accusamus distinctio cupiditate blanditiis dolor? Illo
-          perferendis eveniet cum cupiditate aliquam?
-        </p>
-        <p className="post-date">Posted on 04/16/2019</p>
-      </div>
+const CommentItem = ({
+  comment: { avatar, date, name, text, user, _id },
+  postId,
+  deleteComment,
+  auth,
+}) => (
+  <div className="post bg-white p-1 my-1">
+    <div>
+      <Link to={`/profile/${user}`}>
+        <img className="round-img" src={avatar} alt="" />
+        <h4>{name}</h4>
+      </Link>
     </div>
-  );
+    <div>
+      <p className="my-1">{text}</p>
+      <p className="post-date">
+        Posted on <Moment format="YYYY/MM/DD">{date}</Moment>
+      </p>
+      {!auth.loading && auth.user._id === user && (
+        <button
+          type="button"
+          className="btn btn-danger"
+          onClick={e => deleteComment(postId, _id)}
+        >
+          <i className="fas fa-times" />
+        </button>
+      )}
+    </div>
+  </div>
+);
+
+CommentItem.propTypes = {
+  auth: PropTypes.object.isRequired,
+  comment: PropTypes.object.isRequired,
+  postId: PropTypes.string.isRequired,
+  deleteComment: PropTypes.func.isRequired,
 };
 
-CommentItem.propTypes = {};
+const mapStateToProps = state => ({
+  auth: state.auth,
+});
 
-export default CommentItem;
+export default connect(
+  mapStateToProps,
+  { deleteComment }
+)(CommentItem);
